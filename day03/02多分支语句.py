3b603d8e6e2c0add29d88a3b58f9fa98249654bb
haozhang <799407209@qq.com>
2024-02-04 23:51:02 +0800

    分支语句

---
+++ day03/02多分支语句.py
@@ -0,0 +1,12 @@
+age = input("请输入你的年龄")
+print("5">="18")
+if age >= "18":
+    print("你可以抽烟")
+elif age >= "16":
+    print("你可以喝酒")
+elif age >= "10":
+    print("你可以骑自行车")
+elif age >= "2":
+    print("你可以看喜羊羊")
+else:
+    print("你需要被照顾")
