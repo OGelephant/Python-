99491aa08d35c8a89e054f9401eedbf93934c696
haozhang <799407209@qq.com>
2024-02-06 00:05:22 +0800

    循环语句

--- day03/06随机验证码.py
+++ day03/06随机验证码.py
@@ -7,6 +7,11 @@
 #     print(i)
 
 # range循环 
-
-for i in range(1,100):
-    print(i)
+import random
+import string
+list1 = ''
+allchar = string.ascii_lowercase+string.ascii_uppercase+string.digits
+for i in range(0,4):
+    char1=random.choice(allchar)
+    list1 = list1+char1
+print(list1)
