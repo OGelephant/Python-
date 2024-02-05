f1281a96e42321ac901a5352283d968d845fafea
haozhang <799407209@qq.com>
2024-02-05 23:50:02 +0800

    循环语句

---
+++ day03/04循环语句.py
@@ -0,0 +1,10 @@
+# while循环格式：
+
+# 无限循环
+# while True:
+#     print("Hello world!")
+
+count =0
+while count<10:
+    count=count+1
+    print(count)
---
+++ day03/06随机验证码.py
@@ -0,0 +1,12 @@
+# for循环
+# for i in 容器数据类型
+# s = "abcdef"
+# list = ''
+# for i in s:
+#     print("hello")
+#     print(i)
+
+# range循环 
+
+for i in range(1,100):
+    print(i)
